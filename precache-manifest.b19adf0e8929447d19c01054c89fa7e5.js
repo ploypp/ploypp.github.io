fc8fcf95860ef04ae12d2247a1c2dcf2b955c813
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-02 03:03:42 +0000

    Deploy ploypp/portfolio to github.com/ploypp/portfolio.git:master

---
+++ precache-manifest.b19adf0e8929447d19c01054c89fa7e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab075559391b67159e8b3ea952c30d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9dd783893cc2fab759e",
+    "url": "/static/css/main.72d1ae23.chunk.css"
+  },
+  {
+    "revision": "a1de4d5553f57f8dc774",
+    "url": "/static/js/2.988dfabc.chunk.js"
+  },
+  {
+    "revision": "d9918845f6a4dbfb71d4175c2933cebe",
+    "url": "/static/js/2.988dfabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9dd783893cc2fab759e",
+    "url": "/static/js/main.dc3684e3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
