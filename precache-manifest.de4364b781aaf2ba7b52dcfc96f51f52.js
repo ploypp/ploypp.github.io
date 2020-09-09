db6e162622ab89f86e0f8bbb9477d8b9260b3939
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-09 07:02:43 +0000

    Deploy ploypil.in to github.com/ploypp/ploypp.github.io.git:master

---
+++ precache-manifest.de4364b781aaf2ba7b52dcfc96f51f52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0b8fce07baff9086beedc05d2a4b8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb528899c28257503e4d",
+    "url": "/static/css/main.0f07d624.chunk.css"
+  },
+  {
+    "revision": "a995db6de8356b0125b1",
+    "url": "/static/js/2.93d71dd5.chunk.js"
+  },
+  {
+    "revision": "6a7903cba7c07c04b74cefcf79c6a3f5",
+    "url": "/static/js/2.93d71dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb528899c28257503e4d",
+    "url": "/static/js/main.47e6db1f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
