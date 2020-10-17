b6bb78b69738ea20b00628dc9a9e2906a1674b52
Ploypilin Pruekcharoen <ploypp@Ploypilins-MacBook-Pro.local>
2020-10-17 15:11:09 -0700

    edit resume link

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -6,7 +6,7 @@ export default ({ close }) => (
     <ul>
       <Link to = "/" className="nav-link"><li onClick={close}>Work</li></Link>
       <Link to = "/About" className="nav-link"><li onClick={close}>About</li></Link>
-      <li onClick={close}><a href="https://drive.google.com/file/d/17O-dAamtuhbXWEECaDGEoBR1X7ZMOWOc/view?usp=sharing" target="_blank">Resume</a></li>
+      <li onClick={close}><a href="https://drive.google.com/file/d/1P7A8DSyiqgltpDkaxk7unNgAhzDPcAcX/view?usp=sharing" target="_blank">Resume</a></li>
       <Link to = "/snapanddraw" className="nav-link"><li onClick={close}>Snap&Draw</li></Link>
     </ul>
   </div>
