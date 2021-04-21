97fdf9d8b2b56363014cf5915598791519d465e9
Ploypilin Pruekcharoen <ploypp@Ploypilins-MacBook-Pro.local>
2021-04-21 16:01:00 -0700

    update menu

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -4,10 +4,10 @@ import {Link } from "react-router-dom";
 export default ({ close }) => (
   <div className="Menu">
     <ul>
-      <Link to = "/" className="nav-link"><li onClick={close}>Work</li></Link>
+      <Link to = "/" className="nav-link"><li onClick={close}>UX</li></Link>
+      <Link to = "/artandgraphic" className="nav-link"><li onClick={close}>Art&Graphic</li></Link>
       <Link to = "/About" className="nav-link"><li onClick={close}>About</li></Link>
       <li onClick={close}><a href="https://drive.google.com/file/d/1kn2lm7zzyfCXStlnALBsfcKdJcMVHtZZ/view?usp=sharing" target="_blank">Resume</a></li>
-      <Link to = "/artandgraphic" className="nav-link"><li onClick={close}>Art&Graphic</li></Link>
     </ul>
   </div>
 );
